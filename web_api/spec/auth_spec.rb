2868c6afd4ce60d6547e55d206c33282bba64aab
Jeri Levine <jeri.levine@gmail.com>
2020-10-29 16:13:01 +0000

    a couple of negative tests

--- web_api/spec/auth_spec.rb
+++ web_api/spec/auth_spec.rb
@@ -11,6 +11,13 @@ describe 'auth' do
     expect(resp.code).to eq(200)
   end
 
+  it 'will return an error when invalid credentials are sent via POST' do
+    payload = { username: username, password: 'badpassword' }.to_json
+    req = RestClient::Request.new(method: :post, url: "#{url}/login", payload: payload,
+                                  headers: { content_type: 'application/json' })
+    expect { req.execute }.to raise_error(RestClient::Forbidden)
+  end
+
   it 'can clear a token via POST' do
     payload = { token: token }.to_json
     resp = RestClient::Request.execute(method: :post, url: "#{url}/logout", payload: payload,
@@ -23,5 +30,12 @@ describe 'auth' do
     resp = RestClient::Request.execute(method: :post, url: "#{url}/validate", payload: payload,
                                       headers: { content_type: 'application/json' })
     expect(resp.code).to eq(200)
+  end
+
+  it 'will return an error on an invalid token via POST' do
+    payload = { token: 'badtoken' }.to_json
+    req = RestClient::Request.new(method: :post, url: "#{url}/validate", payload: payload,
+                                  headers: { content_type: 'application/json' })
+    expect { req.execute }.to raise_error(RestClient::Forbidden)
   end
 end
